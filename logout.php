78c865ba6e3b73f7208a098dbfa4483e35a16124
sarushinde302 <sarushinde302@gmail.com>
2025-02-28 11:28:36 +0530

    Added header to logout.php for DevOps practical

--- logout.php
+++ logout.php
@@ -2,4 +2,5 @@
 session_start();
 unset($_SESSION["user"]);
 header("location:index.php");
+<h2 class="text-uppercase text-green font-weight-bold font-style-underline">Edited by Saraswati for DevOps Practical</h2>
 ?>
